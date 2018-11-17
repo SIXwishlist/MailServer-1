0f53a81724f29ecc8429618fb266e06d6905961d
sughas <sughas.ramakrishnan@gmail.com>
2018-11-17 22:28:48 +0530

    Add files via upload

---
+++ Mailserver/aboutus.php
@@ -0,0 +1,9 @@
+<h2 style="background:#99FF33;padding:5px" align="left">About us</h2>
+<p> 
+Developed by Hemanth D R and Sughas R K.</p>
+
+<p>Our project was mainly developed by Sughas
+ </p>
+
+
+
